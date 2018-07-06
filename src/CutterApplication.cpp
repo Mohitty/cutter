b03f602450fadef51208f5725dc256a7cd5b229c
xarkes <antide.petit@gmail.com>
2018-07-06 19:16:57 +0200

    Cleanup

--- src/CutterApplication.cpp
+++ src/CutterApplication.cpp
@@ -236,7 +236,7 @@ void CutterApplication::loadPlugins()
         }
     }
 
-    qDebug() << "Loaded" << plugins.length() << "plugins.";
+    qInfo() << "Loaded" << plugins.length() << "plugins.";
     Core()->setCutterPlugins(plugins);
 }
 
